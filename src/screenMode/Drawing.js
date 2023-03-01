1528a5b4a68412de446a1aaf81e2d5bfc335571b
Changhun Han <hun2487@naver.com>
2023-03-01 20:46:17 +0900

    텍스트입력구현, Polygon 구현중

--- src/screenMode/Drawing.js
+++ src/screenMode/Drawing.js
@@ -1,11 +1,11 @@
 import React from "react";
-import {useRef, useState, useEffect, createElement} from "react";
+import {useRef, useState, useEffect} from "react";
 import { Layout } from "antd";
 import "./canvas.css"
 import lineimg from './line.png'
 import recimg from './rectangle.png'
+import textimg from './text.png'
 import 'tui-grid/dist/tui-grid.css'
-import Attribute from "../attribute/grid";
 
 const { Content} = Layout;
 
@@ -21,8 +21,9 @@ function Drawing(){
     const [recpos, setRecPos] = useState([]); // rectangle 위치
     const [linepos, setLinePos] = useState([]); // line 위치
     const [isDraw, setIsDraw] = useState(false); //isDrawing
-
-    const font = '14px sans-serif';
+    const [text, setText] = useState();
+    const [polygon, setPolygon] = useState([]);
+    const a = null;
 
     useEffect(() => {
         const canvas = canvasRef.current;
@@ -51,6 +52,33 @@ function Drawing(){
         ctx.closePath();
     }
 
+    function drawPolygon(e){
+        let currentX = e.clientX - canvasRef.current.offsetLeft;
+        let currentY = e.clientY - canvasRef.current.offsetTop;
+        setIsDraw(true);
+        setPolygon([e.clientX - canvasRef.current.offsetLeft, e.clientY - canvasRef.current.offsetTop])
+        //버튼 눌렀을 때 시작위치
+        ctx.lineWidth = 3;
+        ctx.strokeStyle = color;
+        console.log(polygon);
+        ctx.beginPath()
+        ctx.moveTo(currentX, currentY);
+        ctx.lineTo(polygon[0], polygon[1]); //마우스 땠을 때 좌표랑 이어줌.
+        ctx.stroke();
+        ctx.closePath();
+    }
+
+    function drawPolygonPoint(e){
+        if(!isDraw) return;
+        
+        setIsDraw(false);
+        ctx.closePath();
+    }
+
+    function drawLineEnd(e){
+        ctx.lineto()
+    }
+
     function drawRecStart(e){
         setIsDraw(true);
         setRecPos([e.clientX - canvasRef.current.offsetLeft, e.clientY - canvasRef.current.offsetTop])
@@ -66,64 +94,49 @@ function Drawing(){
         setIsDraw(false);
     }
 
-    function handleCanvasClick(e){
-        if(!isDraw) return;
-        addInput(e.clientX, e.clientY);
-    }
-
-    function addInput(x,y){
-        const input = createElement("input",
-        {type:"text", onFocus:true ,onKeyDown:handleEnter,style:{position:"fixed", left:`${x-4}px`, top:`${y-4}px`}});
-        //canvasRef.current.child
-        //input.type = "text";
-        //input.style.position = "fixed";
-        //input.style.left = `${x-4}px`;
-        //input.style.top = `${y-4}px`;
-        canvasRef.current.appendChild(input);
-        //mouse.current.
-        //input.onkeyDown = handleEnter;
-        //document.getElementById("canvas").appendChild($input);
-        //document.div.appendChild($input);
-        // input.focus();
-        setIsDraw(true);
-        console.log(canvasRef);
+    function AddInput(){
+        return (
+            <input
+            style={{width:"200px"}}
+            onKeyDown={handleEnter}            
+            />
+        );
     }
 
     function handleEnter(e){
         const keyCode = e.keyCode;
-        if(keyCode === "Enter"){
-            drawText(e.target.value, parseInt(e.target.style.left, 10), parseInt(e.target.top, 10));
-        document.getElementById("canvas").removeChild(e.target);
-        setIsDraw(false)
+        if(keyCode === 13){
+            setText(e.target.value);
         };
     }
 
-    function drawText(txt, x, y){
+    function drawText(e){
+        let currentX = e.clientX - canvasRef.current.offsetLeft;
+        let currentY = e.clientY - canvasRef.current.offsetTop;
         ctx.textBaseline = "top";
         ctx.textAligh = "left";
         ctx.font = "14px sans-serif";
-        ctx.fillText(txt, x-4, y-4);
+        ctx.fillText(text, currentX, currentY);
+        //setIsDraw(false)
     }
-
-    
-
     return (
     <Layout>
         <div className="ColorControl" style={{float:"right", padding:5, width:"50%"}}>
                 <button className="color-btn" data-color="black" onClick={() => setColor("black")}></button>
                 <button className="color-btn" data-color="red" onClick={() => setColor("red")}></button>
                 <button className="color-btn" data-color="green" onClick={() => setColor("green")}></button>
-            <button className="color-btn" data-color="blue" onClick={() => setColor("blue")}></button>
+                <button className="color-btn" data-color="blue" onClick={() => setColor("blue")}></button>
         </div>
         <div className="ModeControl" style={{float:"left" , width: "50%"}}>
                 <img src={lineimg} width='30' height='30' onClick={() => setMode("line")}></img>
                 <img src={recimg} width='30' height='30' onClick={() => setMode("rectangle")}></img>
-                <button className="color-btn" data-color="green" onClick={() => setMode("text")}></button>
+                <img src={textimg} width='30' height='30'  onClick={() => setMode("text")}></img>
+                <button className="color-btn" data-color="blue" onClick={() => setMode("polygon")}></button>
         </div>
+        <AddInput />
         <Content style={{margin:10 ,padding: "0 20px"}}>
         <div style={{background:"#fff", padding:24, minHeight:500}}>
-                    <canvas id="canvas" style={canvasStyle} ref={canvasRef} width={500} height={500} onClick={addInput}
-                        
+                    <canvas id="canvas" style={canvasStyle} ref={canvasRef} width={500} height={500} 
                         onMouseDown={(e) =>
                             {
                                 if(mode ==="line"){
@@ -131,8 +144,10 @@ function Drawing(){
                                 }else if(mode === "rectangle"){
                                     drawRecStart(e);
                                 }else if(mode === "text"){
-                                    handleCanvasClick(e);
-                                    addInput(6,6);
+                                    //handleEnter(e);
+                                    drawText(e);
+                                }else if(mode ==="polygon"){
+                                    drawPolygon(e);
                                 }
                             }
                         }
@@ -143,7 +158,9 @@ function Drawing(){
                                 }else if(mode === "rectangle"){
                                     drawRecEnd(e);
                                 }else if(mode === "text"){
-                                    handleCanvasClick(e);
+                                    handleEnter(e);
+                                }else if(mode === "polygon"){
+                                    drawPolygonPoint(e);
                                 }
                             }
                         }
@@ -153,9 +170,7 @@ function Drawing(){
                     /> 
                     <div ref={mouse}></div>
                 </div>
-                
         </Content>
-        
     </Layout>
     
     );
